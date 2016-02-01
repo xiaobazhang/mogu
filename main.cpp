50b64426b2fc26286e3ab091b15b8e4935223fa2
xiaobazhang <247933092@qq.com>
2016-02-01 11:11:25 +0800

    update

--- main.cpp
+++ main.cpp
@@ -4,46 +4,18 @@
 #include "test.h"
 using namespace ckit;
 
-int main(void)
+int main()
 {
 	//ckit::log::SetPrintLogger();
-	Test myConsumer,myConsumer1,myConsumer2,myConsumer3;
+	Test myConsumer;
 	myConsumer.SetTopicConf("topic.offset.store.method", "file");
 	myConsumer.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
 	myConsumer.SetTopicConf("auto.commit.interval.ms", "10");
 	myConsumer.SetTopicConf("group.id", "g1");
 	myConsumer.SetTopic("msearch_cpc");
 	myConsumer.SetPartition(0);
-
-	myConsumer1.SetTopicConf("topic.offset.store.method", "file");
-	myConsumer1.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
-	myConsumer1.SetTopicConf("auto.commit.interval.ms", "10");
-	myConsumer1.SetTopicConf("group.id", "g1");
-	myConsumer1.SetTopic("msearch_cpc");
-	myConsumer1.SetPartition(1);
-
-	myConsumer2.SetTopicConf("topic.offset.store.method", "file");
-	myConsumer2.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
-	myConsumer2.SetTopicConf("auto.commit.interval.ms", "10");
-	myConsumer2.SetTopicConf("group.id", "g1");
-	myConsumer2.SetTopic("msearch_cpc");
-	myConsumer2.SetPartition(2);
-
-	myConsumer3.SetTopicConf("topic.offset.store.method", "file");
-	myConsumer3.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
-	myConsumer3.SetTopicConf("auto.commit.interval.ms", "10");
-	myConsumer3.SetTopicConf("group.id", "g1");
-	myConsumer3.SetTopic("msearch_cpc");
-	myConsumer3.SetPartition(3);
-
 	myConsumer.Start();
-	myConsumer1.Start();
-	myConsumer2.Start();
-	myConsumer3.Start();
 	myConsumer.Join();
-	myConsumer1.Join();
-	myConsumer2.Join();
-	myConsumer3.Join();
 	while(1)
 	{
 		sleep(1); 
