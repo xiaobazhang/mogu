940310967c4f5078238fa09bc27f2956f574760d
xiaobazhang <247933092@qq.com>
2016-02-02 13:11:14 +0800

    update

--- main.cpp
+++ main.cpp
@@ -8,21 +8,48 @@ using namespace std;
 int main()
 {
 	//ckit::log::SetPrintLogger();
-	char group[4][8] = {"g1","g2","g3","g4"};
-	for(int i=0;i<4;i++)
-	{
-		HandleMessage *myHandleMessage= new HandleMessage();
-		myHandleMessage->SetTopicConf("topic.offset.store.method", "file");
-		myHandleMessage->SetTopicConf("topic.offset.store.sync.interval.ms", "1");
-		myHandleMessage->SetTopicConf("auto.commit.interval.ms", "10");
-		myHandleMessage->SetTopicConf("group.id", group[i]);
-		std::cout<<group[i]<<std::endl;
-		myHandleMessage->SetOffset(RD_KAFKA_OFFSET_END);
-		myHandleMessage->SetTopic("msearch_cpc");
-		myHandleMessage->SetPartition(i);
-		myHandleMessage->Start();
-		myHandleMessage->Join();
-	}
+	HandleMessage myHMess0,myHMess1,myHMess2,myHMess3;
+	myHMess0.SetTopicConf("topic.offset.store.method", "file");
+	myHMess0.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
+	myHMess0.SetTopicConf("auto.commit.interval.ms", "10");
+	myHMess0.SetTopicConf("group.id","g1");
+	myHMess0.SetOffset(RD_KAFKA_OFFSET_END);
+	myHMess0.SetTopic("msearch_cpc");
+	myHMess0.SetPartition(0);
+
+	myHMess1.SetTopicConf("topic.offset.store.method", "file");
+	myHMess1.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
+	myHMess1.SetTopicConf("auto.commit.interval.ms", "10");
+	myHMess1.SetTopicConf("group.id","g2");
+	myHMess1.SetOffset(RD_KAFKA_OFFSET_END);
+	myHMess1.SetTopic("msearch_cpc");
+	myHMess1.SetPartition(1);
+
+	myHMess2.SetTopicConf("topic.offset.store.method", "file");
+	myHMess2.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
+	myHMess2.SetTopicConf("auto.commit.interval.ms", "10");
+	myHMess2.SetTopicConf("group.id","g3");
+	myHMess2.SetOffset(RD_KAFKA_OFFSET_END);
+	myHMess2.SetTopic("msearch_cpc");
+	myHMess2.SetPartition(2);
+
+	myHMess3.SetTopicConf("topic.offset.store.method", "file");
+	myHMess3.SetTopicConf("topic.offset.store.sync.interval.ms", "1");
+	myHMess3.SetTopicConf("auto.commit.interval.ms", "10");
+	myHMess3.SetTopicConf("group.id","g4");
+	myHMess3.SetOffset(RD_KAFKA_OFFSET_END);
+	myHMess3.SetTopic("msearch_cpc");
+	myHMess3.SetPartition(3);
+
+	myHMess0.Start();
+	myHMess0.Join();
+	myHMess1.Start();
+	myHMess1.Join();
+	myHMess2.Start();
+	myHMess2.Join();
+	myHMess3.Start();
+	myHMess3.Join();
+
 	Test myConsumer;
 	myConsumer.Start();
 	myConsumer.Join();
