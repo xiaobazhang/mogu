5564a406453ec003620d6acfee26429404283a58
xiaobazhang <247933092@qq.com>
2016-02-04 15:05:52 +0800

    update

--- test.cpp
+++ test.cpp
@@ -53,11 +53,6 @@ void Test::CountLog(const string& strlog,map<string,map<int,int> >& mapcount)
 		else
 			mapcount["SearchZero"][iCurrentTime] = 1;
 	}
-	else
-	{
-
-		mapcount["SearchZero"][iCurrentTime] = 0;
-	}
 	if(IsSearchFailed(strlog))
 	{
 		if(mapcount["SearchFaild"].count(iCurrentTime))
@@ -65,22 +60,13 @@ void Test::CountLog(const string& strlog,map<string,map<int,int> >& mapcount)
 		else
 			mapcount["SearchFaild"][iCurrentTime] = 1;
 	}
-	else
-	{
-		mapcount["SearchFaild"][iCurrentTime] = 0;
-	}
 	if(IsSearchDiscard(strlog))
 	{
 		if(mapcount["SearchDiscard"].count(iCurrentTime))
 			mapcount["SearchDiscard"][iCurrentTime]++;
 		else
 			mapcount["SearchDiscard"][iCurrentTime] = 1;
-	}
-	else
-	{
-		mapcount["SearchDiscard"][iCurrentTime] = 0;
-	}
-		
+	}	
 }
 /**
  * @AuthorHTL
