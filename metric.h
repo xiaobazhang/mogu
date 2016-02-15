3ec6a3af03ca2af209ca2da27463e9adc2a01709
xiaobazhang <247933092@qq.com>
2016-02-15 14:16:03 +0800

    update

--- metric.h
+++ metric.h
@@ -34,7 +34,7 @@ private:
 class Metric
 {
 public:
-	Metric():miQueueMaxNum(10){}
+	Metric():miQueueMaxNum(6){}
 	~Metric(){}
 	void HandleMetric(const string strmetric,const string strhost,int itime,int ivalue);
 	void HandleMetric(const string strmetric,const string strhost,int itime,float fvalue);
