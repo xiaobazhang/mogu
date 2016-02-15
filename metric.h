48ddca073e0749d41c7a747c64c62b7fa13197f7
xiaobazhang <247933092@qq.com>
2016-02-15 10:33:42 +0800

    update

--- metric.h
+++ metric.h
@@ -23,13 +23,7 @@ public:
 	~MessageSend();
 	void SetMessage(string hostname,string str)
 	{
-		string strTime = ckit::time::
-		char buf[512] = {0};
-
-		sprintf(buf,"appName=search&host=%s&method=DEAL&metaData=%s %s http://monitor.bit.service.mogujie.org:8080/alarmcenter/service/alarm",hostname.c_str(),);
-		string str = "appName=search&host=${HOSTNAME}&method=DEAL&metaData=${time} “msg you send” 
-		 "http://monitor.bit.service.mogujie.org:8080/alarmcenter/service/alarm";
-
+		
 	}
 	bool SendMessage();
 private:
