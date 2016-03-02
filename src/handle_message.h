a3fbf1d3894dc972ad67b9c4904fb306b6c79b0c
suli <suli@mogujie.com>
2016-03-02 14:37:48 +0800

    update

--- src/handle_message.h
+++ src/handle_message.h
@@ -7,7 +7,7 @@
 #include <foundation/ckit_lock.h>
 #include <iostream>
 #include "log_analysis.h"
-#include "index_merger.h"
+//#include "index_merger.h"
 
 using namespace std;
 using namespace ckit;
@@ -72,23 +72,21 @@ public:
 private:
 };
 
-
 class HandIndexMess : public LogAnalysis
 {
 public:
 	HandIndexMess()
 	{
-		m_index = new IndexMerger();
-		m_index->SetMaxSize(40,10);
+		m_index.SetMaxSize(40,10);
 	}
 	~HandIndexMess();
 	virtual void Process(rd_kafka_message_t * pMessage)
 	{
 		string data = strRecvMes((char*)pMessage->payload,pMessage->len);
 		string ip = "10.15.15.38";
-		m_index->Process(ip,data);
+		m_index.Process(ip,data);
 	}
 private:
-	IndexMerger* m_index;
+	IndexMerger m_index;
 };
 #endif
