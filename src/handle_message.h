402bb8d51943142456c6fa451a529073040600a0
suli <suli@mogujie.com>
2016-03-02 14:32:02 +0800

    update

--- src/handle_message.h
+++ src/handle_message.h
@@ -78,16 +78,17 @@ class HandIndexMess : public LogAnalysis
 public:
 	HandIndexMess()
 	{
-		//m_index.SetMaxSize(40,10);
+		m_index = new IndexMerger();
+		m_index->SetMaxSize(40,10);
 	}
 	~HandIndexMess();
 	virtual void Process(rd_kafka_message_t * pMessage)
 	{
 		string data = strRecvMes((char*)pMessage->payload,pMessage->len);
 		string ip = "10.15.15.38";
-		m_index.Process(ip,data);
+		m_index->Process(ip,data);
 	}
 private:
-	IndexMerger m_index;
+	IndexMerger* m_index;
 };
 #endif
