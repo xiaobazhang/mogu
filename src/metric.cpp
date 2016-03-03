bd08f70085baaad1ec7d3d603cb8864c6bd63f50
suli <suli@mogujie.com>
2016-03-03 16:11:28 +0800

    update

--- src/metric.cpp
+++ src/metric.cpp
@@ -57,7 +57,7 @@ void Metric::Run()
 			system(char_alarm);
 			delete [] alarm;
 		}
-		ckit::time::SleepByUs(10);//休眠10us
+		ckit::time::SleepByUs(100);//休眠10us
 		int end = ckit::time::GetCurrentUs()-first;
 		//sstd::cout<<"user time "<<end;	
 	}
